9e536b4410e3437592f93b3b38abb72d02c5ead2
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-26 18:43:56 -0300

    feat: add the passport config

---
+++ config/passport-config.js
@@ -0,0 +1,42 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const db = require("../src/models/userModel");
+
+function initializePassport(passport) {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          const user = await db.getUserBy("email", email);
+
+          if (!user) {
+            return done(null, false, { message: "No user found" });
+          }
+
+          const match = await bcrypt.compare(password, user.password);
+          if (!match) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          return done(null, user);
+        } catch (err) {
+          return done(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await db.getUserBy("id", id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
+
+module.exports = initializePassport;
