1e47be0ec8644d291ec43feb68445a1b6724df75
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-27 21:03:08 -0300

    feat: add configuration to use styles

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 
 // Passport
 initializePassport(passport);
