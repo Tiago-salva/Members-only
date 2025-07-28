a21542a986d195a4b7657d6ab5ff21d2ea71596c
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-27 21:04:08 -0300

    refactor: change the "email" user field to "username"

--- config/passport-config.js
+++ config/passport-config.js
@@ -4,27 +4,24 @@ const db = require("../src/models/userModel");
 
 function initializePassport(passport) {
   passport.use(
-    new LocalStrategy(
-      { usernameField: "email" },
-      async (email, password, done) => {
-        try {
-          const user = await db.getUserBy("email", email);
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await db.getUserBy("username", username);
 
-          if (!user) {
-            return done(null, false, { message: "No user found" });
-          }
-
-          const match = await bcrypt.compare(password, user.password);
-          if (!match) {
-            return done(null, false, { message: "Incorrect password" });
-          }
-
-          return done(null, user);
-        } catch (err) {
-          return done(err);
+        if (!user) {
+          return done(null, false, { message: "No user found" });
         }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
-    )
+    })
   );
 
   passport.serializeUser((user, done) => done(null, user.id));
