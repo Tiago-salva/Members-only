f295caa766ec1bc8cae57e623b697fbb71c011c6
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-28 11:45:42 -0300

    feat: add validation for the log-in form

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,13 +4,15 @@ const {
   createUserPost,
   renderLogInForm,
   logOut,
+  membershipStatusGet,
+  membershipStatusPost,
 } = require("../controllers/authController");
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const authRouter = Router();
 const passport = require("passport");
 const { isNotAuthenticated } = require("../middleware/authMiddleware");
 
-// Sign Up
+// Sign up
 authRouter.get("/sign-up", isNotAuthenticated, createUserGet);
 
 authRouter.post(
@@ -29,19 +31,39 @@ authRouter.post(
   createUserPost
 );
 
-// Log In
+// Log in
 authRouter.get("/log-in", isNotAuthenticated, renderLogInForm);
 
 // Agregar validacion
 authRouter.post(
   "/log-in",
+  [
+    body("username").notEmpty().withMessage("The username is obligatory"),
+    body("password").notEmpty().withMessage("The password is obligatory"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("log-in-form", {
+        errors: errors.array(),
+      });
+    }
+
+    next();
+  },
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/log-in",
+    failureFlash: true,
   })
 );
 
-// Log Out
+// Log out
 authRouter.get("/log-out", logOut);
+
+// Membership status
+authRouter.get("/membership", membershipStatusGet);
+
+authRouter.post("/membership", membershipStatusPost);
 
 module.exports = authRouter;
