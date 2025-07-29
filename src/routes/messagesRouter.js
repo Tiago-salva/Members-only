cbf1fe6bb7f2979a8c45785beba7261fa279425d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-29 10:19:55 -0300

    bugFix: fix the missing route in the new message routes

--- src/routes/messagesRouter.js
+++ src/routes/messagesRouter.js
@@ -3,14 +3,17 @@ const {
   getAllMessages,
   createMessageGet,
   createMessagePost,
+  deleteMessage,
 } = require("../controllers/messagesController");
 const { isAuthenticated } = require("../middleware/authMiddleware");
 const messagesRouter = Router();
 
 messagesRouter.get("/messages", getAllMessages);
 
-messagesRouter.get("/new", isAuthenticated, createMessageGet);
+messagesRouter.get("/messages/new", isAuthenticated, createMessageGet);
 
-messagesRouter.post("/new", createMessagePost);
+messagesRouter.post("/messages/new", createMessagePost);
+
+messagesRouter.post("/messages/:id/delete", deleteMessage);
 
 module.exports = messagesRouter;
