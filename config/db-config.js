3edbc0a4db07f262517594eeafc8fcc208b6f109
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-30 09:29:32 -0300

    feat: use dotenv for the connectionString of the db pool

--- config/db-config.js
+++ config/db-config.js
@@ -1,8 +1,8 @@
 const { Pool } = require("pg");
+require("dotenv").config();
+
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgres://postgres:roottree@localhost:5432/members_only",
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
